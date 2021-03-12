73015eda98ccbb0c3b1438c3cf468d089588626e
ffernando-cg <maverick_2806@hotmail.com>
2021-03-12 17:27:40 -0600

    Practica 3 y 4

--- Clase7/script4.js
+++ Clase7/script4.js
@@ -10,47 +10,48 @@ var propiedades = {
 
 
 var iniciaMapa = () => {
-  fetch('./paises.json')
+  fetch('paises.json')
   .then(
-  function(datos){
-      const map = new google.maps.Map(document.getElementById('map'),propiedades);
+  function(response){
+    
+      response.json().then(function(datos){
+        const map = google.maps.Map(document.getElementById('map'),propiedades);
 
-      datos.forEach( marcador => {
-
-        fetch('https://corona.lmao.ninja/v2/countries')
-        .then((res)=>{
-          res.json().then( (datospaises) => {
+        datos.forEach(marcador => {
+          fetch('https://corona.lmao.ninja/v2/countries')
+          .then((res)=>{
+            res.json().then( (datospaises) => {
             
-
-            datospaises.forEach( registro => {
-
-              var informacion = `<strong>PAIS: </strong> ${registro.country}, casos: ${registro.cases} <br/> <img src="${registro.countryInfo.flag}"/>`;
-              var infowindow = new google.maps.InfoWindow({
-                content: informacion
-
-              })
-
-              if( registro.country == marcador.CountryName){
-
-                let marker = new google.maps.Marker({
-                  map: map,
-                  position: new google.maps.LatLng(marcador.countryInfo.lat, marcador.countryInfo.long),
-                  title: `${marcador.CountryName} : ${registro.cases}`
-                });
-
-                marker.addListener('click', function(){
-                  infowindow.open(map,marker);
+              datospaises.forEach( registro => {
+  
+                var informacion = `<strong>PAIS: </strong> ${registro.country}, casos: ${registro.cases} <br/> <img src="${registro.countryInfo.flag}"/>`;
+                var infowindow = new google.maps.InfoWindow({
+                  content: informacion
+  
                 })
-
-              }
-
+  
+                if( registro.country == marcador.CountryName){
+  
+                  let marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(marcador.countryInfo.lat, marcador.countryInfo.long),
+                    title: `${marcador.CountryName} : ${registro.cases}`
+                  });
+  
+                  marker.addListener('click', function(){
+                    infowindow.open(map,marker);
+                  })
+  
+                }
+  
+              })
+  
             })
-
           })
         })
-      });
-  })
-  .catch(
-    (e)=> console.log(`Ocurrio un error: ${e}`)
-  )
-};
+        
+      })
+      }).catch(
+        (e)=> console.log(`Ocurrio un error: ${e}`)
+      );
+  };
