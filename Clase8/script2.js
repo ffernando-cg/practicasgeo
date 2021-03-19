4166eabc0a7879873cab7be877491998c40e5a51
ffernando-cg <maverick_2806@hotmail.com>
2021-03-19 17:17:04 -0600

    Practica 7 y 8 añadidos

--- Clase8/script2.js
+++ Clase8/script2.js
@@ -3,6 +3,16 @@ var coords = {
   lng : -110.956896
 }
 
+const iniciaMapa = () =>{
+  var map = new google.maps.Map(
+      $('#map'),
+      {
+        center:coords,
+        zoom:3
+      }
+    );
+  }
+
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString);
 const idioma = urlParams.get('idioma');
@@ -13,13 +23,3 @@ $('#idioma').val(idioma);
 
 script.src=`https://maps.googleapis.com/maps/api/js?key=AIzaSyAv-m46ebIV9fqkE2fIBGeWunmGmBm2R24&callback=iniciaMapa&language=${idioma}" async defer`
 document.head.appendChild(script);
-
-const iniciaMapa = () =>{
-var map = new google.maps.Map(
-    $('#map'),
-    {
-      center:coords,
-      zoom:3
-    }
-  );
-}
