32ac95274ab72b079f2b9d738c66d36f2ab03db2
ffernando-cg <maverick_2806@hotmail.com>
2021-03-19 18:06:17 -0600

    Practica 7 y 8 añadidos

--- Clase8/script2.js
+++ Clase8/script2.js
@@ -3,6 +3,17 @@ var coords = {
   lng : -110.956896
 }
 
+const queryString = window.location.search
+const urlParams = new URLSearchParams(queryString);
+const idioma = urlParams.get('idioma');
+
+var script = document.createElement('script');
+
+document.getElementById('idioma').value = idioma
+
+script.src=`https://maps.googleapis.com/maps/api/js?key=AIzaSyAv-m46ebIV9fqkE2fIBGeWunmGmBm2R24&callback=iniciaMapa&language=${idioma}`
+document.head.appendChild(script);
+
 var iniciaMapa = () =>{
   var map = new google.maps.Map(
       document.getElementById('map'),
@@ -12,14 +23,3 @@ var iniciaMapa = () =>{
       }
     );
   }
-
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString);
-const idioma = urlParams.get('idioma');
-
-var script = document.createElement('script');
-
-document.getElementById('idioma').value = idioma
-
-script.src=`https://maps.googleapis.com/maps/api/js?key=AIzaSyAv-m46ebIV9fqkE2fIBGeWunmGmBm2R24&callback=iniciaMapa&language=${idioma}" async defer`
-document.head.appendChild(script);
