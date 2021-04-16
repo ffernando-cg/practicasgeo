892ce86e31e00366164b49d7e1e8724b7801e83f
ffernando-cg <maverick_2806@hotmail.com>
2021-04-16 17:40:26 -0500

    Practica 12 realizada

--- Clase12/js/customScript.js
+++ Clase12/js/customScript.js
@@ -11,10 +11,34 @@ $('#btnSearch').click(function(){
 
   service.findPlaceFromQuery(request, function(res, status){
     if(satuts === google.maps.places.PlacesServiceStatus.OK){
-
+      for (let i = 0; i < res.length.length; i++) {
+        createMarker(res[i]);
+      }
+      map.setCenter(res[0].geometry.location)
     }
   })
 });
+
+function createMarker(place){
+
+  var icon = {
+    url: place.icon,
+    scaledSize: new google.maps.Size(25,25),
+    origin: new google.maps.Point(0,0),
+    anchor: new google.maps.Point(0,0)
+  }
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function(){
+    infoWindow.setContent(`${place.name} \n ${place.formatted_address}`);
+    infoWindow.open(map, this);
+  });
+
+}
 
 function iniciaMapa() {
   var coords= { lat:21.152639, lng: -101.711598 }
